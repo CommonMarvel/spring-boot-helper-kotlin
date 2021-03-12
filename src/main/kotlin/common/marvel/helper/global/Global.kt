2b7600c68c089ea860c8a38946a1b8c0fd1da8a6
ibmisibm <ibmisibm@gmail.com>
2021-03-12 18:02:40 +0800

    feat: customize JavaTimeModule of ObjectMapper

--- src/main/kotlin/common/marvel/helper/global/Global.kt
+++ src/main/kotlin/common/marvel/helper/global/Global.kt
@@ -4,16 +4,31 @@ import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalTimeSerializer
 import com.fasterxml.jackson.module.kotlin.KotlinModule
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.LocalTime
+import java.time.format.DateTimeFormatter
 
 /**
  * @author leo
  */
 object Global {
+    private val javaTimeModule = JavaTimeModule()
+        .addSerializer(
+            LocalDateTime::class.java,
+            LocalDateTimeSerializer(DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSSSS'Z'"))
+        )
+        .addSerializer(LocalDate::class.java, LocalDateSerializer(DateTimeFormatter.ISO_DATE))
+        .addSerializer(LocalTime::class.java, LocalTimeSerializer(DateTimeFormatter.ISO_TIME))
+
     val objectMapper = jacksonObjectMapper()
         .registerModule(KotlinModule())
-        .registerModule(JavaTimeModule())
+        .registerModule(javaTimeModule)
         .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
         .configure(DeserializationFeature.FAIL_ON_NULL_FOR_PRIMITIVES, true)
         .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
